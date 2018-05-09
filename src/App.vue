b72125f2008662a086ffa8a42ff1441fbb34df59
Daniel Meechan <daniel.meechan@ibm.com>
2018-05-09 14:49:17 +0100

    Sorted out Vue templating

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-      <h1>NBS Open Banking Hackathon - OB1-NEM-OB</h1>
-      <img src="./assets/logo.png">
-      <p>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">OB1-NEM-OB</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+
+      
+      <ul class="right hide-on-small-only">
+        <li>
           <router-link to="/">Home</router-link>
-          <router-link to="/hello">Hello</router-link>
-      </p>
-      <router-view></router-view>
+        </li>
+        <li>
+          <router-link to="/login">Login</router-link>
+        </li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/login">Login</router-link>
+        </li>
+      </ul>
+
+</div>
+    </nav>
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
     name: "App",
+
+    created() {
+        $(document).ready(function() {
+            $(".button-collapse").sideNav();
+        });
+    },
 };
 </script>
 
 <style>
-#app {
+/* #app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
-}
+} */
 </style>
