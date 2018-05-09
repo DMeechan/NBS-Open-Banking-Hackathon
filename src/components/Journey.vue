a3047ace2b70cfa6a73b68c7adebb5ee4d019460
Bradley Dale <bradley.daleibm.com@Bradleys-MacBook-Pro.local>
2018-05-09 17:01:48 +0100

    More features on journey page

--- src/components/Journey.vue
+++ src/components/Journey.vue
@@ -1,15 +1,31 @@
 <template>
       <ul class="collapsible">
-        <li class="active">
-            <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+        <li>
+            <div class="collapsible-header active"><i class="material-icons">account_balance</i>Long term</div>
+            <div class="collapsible-body" style="padding-bottom:50px">
+            
+            <span >Savings towards a house
+                 <img class="right" height="30px" src="http://www.processcodi.com/wp-content/uploads/2017/10/easy-house-drawing-house-line-drawing-8-easy-home-design-app.jpg">
+            </span>
+            </div>
+            <div class="collapsible-body" style="padding-bottom:50px">
+            <span >Savings towards a car
+                 <img class="right" height="30px" src="http://clipartoons.com/wp-content/uploads/2016/03/toy-car-clipart-black-and-white-car-clip-art-1.png">
+            </span>
+            </div>
+            <div class="collapsible-body" style="padding-bottom:50px">
+            <span >Savings towards a holiday
+                 <img class="right" height="30px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHMpRXqq8NhODpvJXNkmHrElnwyO1EYvY0VklGJ_hKyTHbou_Q">
+            </span>
+            </div>
+
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">attach_money</i>Savings</div>
             <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
         </li>
         <li>
-            <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-        </li>
-        <li>
-            <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+            <div class="collapsible-header"><i class="material-icons">account_balance_wallet</i>Daily expense</div>
             <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
         </li>
   </ul>
@@ -28,8 +44,10 @@ export default {
 
   $(document).ready(function(){
     $('.collapsible').collapsible();
+
   });
-        
+    
+ 
 </script>
 
 <style scoped>
